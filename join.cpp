a72fc924d636707de6b4cf5b37feaad5b1bed7de
Sanek <a.marin30692@gmail.com>
2018-01-31 19:29:59 +0300

    january

--- join.cpp
+++ join.cpp
@@ -1,12 +1,10 @@
-#include <iostream>
-#include <vector>
 void join(const std::vector<int> &from, std::vector<int> &to)
 {
     std::vector<int> temp (to);
-    to.resize(from.size()+temp.size(), 0);
-    std::vector<int>::const_iterator it_from = from.begin();
+    to.resize(from.size()+temp.size(), 0);                                                                       //complement vector with zeros
+    std::vector<int>::const_iterator it_from  = from.begin();
     std::vector<int>::iterator it_temp = temp.begin(), it_to = to.begin();
-    while (it_from != from.end() && it_temp != temp.end())
+    while (it_from != from.end() && it_temp != temp.end())                                //until the end of one of vectors
     {
         if (*it_from < *it_temp)
         {
@@ -19,7 +17,7 @@ void join(const std::vector<int> &from, std::vector<int> &to)
             ++it_temp, ++it_to;
         }
     }
-    if (it_from == from.end())
+    if (it_from == from.end())                                                                                                //this part copies the rest of the last vector
     {
         std::copy(it_temp, temp.end(), it_to);
     }
